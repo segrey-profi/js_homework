d0249e08b0713a17febe03c4bcf41a44d0b7b631
Артемий Прокофьев <artem@MacBook-Pro-Artemij.local>
2020-01-23 11:53:36 +0300

    init

---
+++ 01/limitCalls.js
@@ -0,0 +1,23 @@
+/**
+ * Напишите функцию limitCalls(fn, maxCalls), принимающую функцию fn,
+ * и возвращающую новую функцию, которую можно вызвать не более
+ * указанного в maxCalls количества раз.
+ *
+ * В целях упрощени, входящая функция не принимает никаких аргументов, работу с
+ * аргументами можно не учитывать.
+ *
+ * Пример:
+ * const limitedLog = limitCalls(() => console.log('log'), 2);
+ * limitedLog(); // 'log'
+ * limitedLog(); // 'log'
+ * limitedLog(); // undefined
+ * limitedLog(); // undefined
+ *
+ *
+ * @param  {Function} fn функция
+ * @param  {number} maxCalls максимальное количество вызовов
+ * @return {Function}
+ */
+export function limitCalls(fn, maxCalls) {
+
+}
