9a273acfa2a9bb9ed1eff81288480de40ad9f0ed
Sergey Markin <markinsv@profi.ru>
2020-02-05 13:35:07 +0300

    03: promiseAll done

--- 03/promiseAll.js
+++ 03/promiseAll.js
@@ -6,5 +6,20 @@
  * @return {Promise}
  */
 export const promiseAll = promises => new Promise((resolve, reject) => {
-
+  const totalCount = promises.length;
+  const results = new Array(totalCount);
+  let hasError = false;
+  let finishedCount = 0;
+  for (let i = 0; i < totalCount; ++i) {
+    promises[i].then(res => {
+      if (hasError) return;
+      results[i] = res;
+      finishedCount += 1;
+      if (finishedCount == totalCount) resolve(results);
+    }).catch(err => {
+      if (hasError) return;
+      hasError = true;
+      reject(err);
+    });
+  }
 });
