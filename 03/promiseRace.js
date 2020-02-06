8906c20237bc29efe490497c6c24c7fe7f40517b
Sergey Markin <markinsv@profi.ru>
2020-02-06 15:40:54 +0300

    03: promiseRace done

--- 03/promiseRace.js
+++ 03/promiseRace.js
@@ -5,6 +5,20 @@
  * @param  {Promise[]} promises массив с исходными промисами
  * @return {Promise}
  */
-export const promiseRace = promises => new Promise((resolve, reject) => {
+const promiseRace = promises => new Promise((resolve, reject) => {
+  let hasFinished = false;
 
+  function processIfRunning(fn) {
+    if (hasFinished) return;
+    hasFinished = true;
+    fn();
+  }
+
+  for (promise of promises) {
+    promise.then(res => {
+      processIfRunning(() => { resolve(res); });
+    }).catch(err => {
+      processIfRunning(() => { reject(err); });
+    });
+  }
 });
